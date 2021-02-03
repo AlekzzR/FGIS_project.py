8323c8912645c411b5d5fd9eff71c99155b3b2ff
AlekzzR <alekzzr@gmail.com>
2021-02-03 19:52:21 +0300

    Добавляю описание

---
+++ main.py
@@ -0,0 +1,31 @@
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    """
+    Что нужно сделать:
+    1. На экране должен быть открыт личный кабинет ФГИС:
+        http://10.250.74.17/lk
+    2. Далее должен быть выбрана кнокпа слева:
+        <span _ngcontent-c20="" class="text">РЕЗУЛЬТАТЫ ДЕЯТЕЛЬНОСТИ</span>
+    3. Если на экране "Доступ запрещен" (http://10.250.74.17/accessdenied):
+        перейти по ссылке: http://10.250.74.17/rpi
+    4. В поле номер протокола (<input _ngcontent-c41=""
+                                autocomplete="off"
+                                class="column-search-panel__text-input
+                                ng-untouched ng-pristine ng-valid"
+                                name="search" type="text">)
+        ввести номер протокола-шаблона (например П-1234/21-54321)
+    5. Выбрать найденный протокол, нажав на него:
+        <div _ngcontent-c46="" class="limited-text-cell__text">имя_протокола</div>,
+            где имя_протокола - это имя из пункта 4
+    6.
+
+    :return:
+    """
+    """if __name__ == '__main__':
+        print_hi('PyCharmу')
+
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+"""
+main()
